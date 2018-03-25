3b279f2a59c6c7806fa02091ddc314ac1e1ed92d
youyun <superyouyun@gmail.com>
2018-03-26 03:21:38 +0800

    graph 3

--- Mini-Project2/D3/js/graph.js
+++ Mini-Project2/D3/js/graph.js
@@ -20,7 +20,7 @@ function drawObj1() {
 
   // load the csv and create the chart
   d3.csv("data/obj1.csv", function(d, i, columns) {
-  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+    for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
     d.total = t;
     return d;
   }, function(error, data) {
@@ -117,8 +117,76 @@ function drawObj1() {
 };
 
 function drawObj2() {
-};
+}
 
 function drawObj3() {
-  
+  var margin = {top: 20, right: 20, bottom: 120, left: 100},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .1, 1),
+    y = d3.scale.linear().range([height, 0]),
+    xAxis = d3.svg.axis().scale(x).orient("bottom"),
+    yAxis = d3.svg.axis().scale(y).orient("left");
+
+  var colors = d3.scale.category20c();
+
+  var svg = d3.select("#graph").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong>Amount:</strong>" + 
+             "Total: " + d.counts + 
+             "<br>Authors: " + d.authors +
+             "<br>inCitations: " + d.inCitations +
+             "<br>outCitations: " + d.outCitations + "</span>";
+    })
+
+  svg.call(tip);
+
+  d3.csv("data/obj3.csv", function(error, data) {
+    x.domain(data.map(function(d) { return d.year; }));
+    y.domain([0, d3.max(data, function(d) { return d.counts; })]);
+    
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll('text')
+        .attr("y", -5)
+        .attr("x", -10)
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "end");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -60)
+        .attr("x", -100)
+        .attr("dy", ".3em")
+        .style("text-anchor", "end")
+        .style("font-size", "16px")
+        .text("Total Publications");
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.year); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.counts); })
+        .attr("height", function(d) { return height - y(d.counts); })
+        .attr("fill", function (d){ return colors(d.year); })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+  });
 };

e9a15b632460efbb643296ee61e0f675d71751d6
youyun <superyouyun@gmail.com>
2018-03-26 00:11:58 +0800

    data processed

--- Mini-Project2/D3/js/data.js
+++ Mini-Project2/D3/js/data.js
@@ -8,7 +8,9 @@ var languagesObj = [];
 
 /*functions to load data*/
 function loadData() {
-    
+    $.getJSON("../Mini-project-2-data.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
 }
 
 
